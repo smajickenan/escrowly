4507b4246753c220c54948a9009aef044476e5c8
dacentra <fahad@dacentra.com>
2025-04-04 17:07:42 +0800

    Update HowItWorksPage.jsx

--- src/pages/HowItWorksPage.jsx
+++ src/pages/HowItWorksPage.jsx
@@ -9,7 +9,7 @@ import Guidlines from "../sections/Guidlines/Guidlines";
 const HomePage = () => (
     <>
         <Helmet>
-            <title>Best Crypto Escrow - Escrowly</title>
+            <title><title>Escrowly - How it Works</title>
             <meta name="description" content="Secure your crypto transactions with Escrowly, the trusted escrow service for crypto traders." />
             <meta name="keywords" content="secure crypto escrow, secure transactions, Escrowly, Bitcoin escrow, Ethereum escrow, crypto escrow service, smart contract escrow, blockchain escrow service, crypto escrow for businesses, USDT escrow service, escrow wallet for crypto, secure P2P crypto escrow, escrow for digital items, escrow for domain names, escrow for real estate" />
             <meta name="author" content="Escrowly Crypto Escrow Team" />
