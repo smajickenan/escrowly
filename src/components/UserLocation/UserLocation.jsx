df2e9575a9808ef2ae362481a0ec905998b75bf0
Kenan Smajic <ksmajic996@gmail.com>
2025-04-10 22:11:42 +0200

    flags

--- src/components/UserLocation/UserLocation.jsx
+++ src/components/UserLocation/UserLocation.jsx
@@ -7,11 +7,16 @@ const UserLocation = () => {
     useEffect(() => {
         const fetchUserLocation = async () => {
             try {
-                const response = await fetch(`https://ipinfo.io/json?token=${process.env.REACT_APP_IPINFO_TOKEN}`);
+                const response = await fetch('https://ipinfo.io/json?token=bc22631ccde9e3');
                 const data = await response.json();
                 console.log('IPINFO API Response:', data);
-                const code = data.country.toLowerCase();
-                setCountryCode(code);
+                if (data.country) {
+                    const code = data.country.toLowerCase();
+                    console.log('Country Code:', code);
+                    setCountryCode(code);
+                } else {
+                    console.error('No country code in response:', data);
+                }
             } catch (error) {
                 console.error('Error fetching location:', error);
             }
@@ -20,14 +25,20 @@ const UserLocation = () => {
         fetchUserLocation();
     }, []);
 
-    if (!countryCode) return null;
+    if (!countryCode) {
+        console.log('No country code set');
+        return null;
+    }
+
+    const flagUrl = `https://flagcdn.com/24x18/${countryCode}.png`;
+    console.log('Flag URL:', flagUrl);
 
     return (
         <div className="user-location">
             <span className="separator">|</span>
             <div className="user-flag">
                 <img 
-                    src={`https://flagcdn.com/24x18/${countryCode}.png`}
+                    src={flagUrl}
                     srcSet={`https://flagcdn.com/48x36/${countryCode}.png 2x,
                             https://flagcdn.com/72x54/${countryCode}.png 3x`}
                     alt={`${countryCode} flag`}
