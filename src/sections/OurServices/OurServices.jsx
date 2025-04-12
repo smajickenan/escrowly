a672e5607311504bd0eb2a4904126b80db89f2b8
dacentra <fahad@dacentra.com>
2025-04-12 20:21:35 +0000

    front page6

--- src/sections/OurServices/OurServices.jsx
+++ src/sections/OurServices/OurServices.jsx
@@ -127,7 +127,7 @@ const OurServices = () => {
                                     <ServiceFeatureCard
                                         icon="/img/service-feature-1.svg"
                                         title="Escrow for Property Sales"
-                                        text="Escrowly.com services caters to property sales and real estate closings for domestic and cross-border transactions. Our services encompasses conventional escrow functions which operates with the same digital precision and security"
+                                        text="Escrowly services caters to property sales and real estate closings for domestic and cross-border transactions to international clients. Our services encompasses conventional escrow functions which operates with the same digital precision and security"
                                         className="feature-blue"
                                     />
                                 </motion.div>
