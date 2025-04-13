aafebdab5057c68170cec732dc7f7f0025bf2323
dacentra <fahad@dacentra.com>
2025-04-13 15:57:14 +0000

    allowed20

--- src/pages/WhatIsEscrowly/AllowedGoodAndServices.jsx
+++ src/pages/WhatIsEscrowly/AllowedGoodAndServices.jsx
@@ -46,12 +46,12 @@ const AllowedGoodsAndServices = () => {
                                         <p>Escrowly is an all-purpose crypto escrow platform that provides a single gateway to secure verified transactions in over thirty business sectors. Our smart contract escrow technology, dispute resolution, and blockchain-backed protection guarantee closing high-value equipment deals.</p>
                                         <h3>Cryptocurrencies and other Digital Assets</h3>
                                         <p>In this age of trading digital assets, the need for a secure exchange platform has become higher than ever. With Escrowly, you can safely trade all major cryptocurrencies through smart contract system tracking, which provides maximum protection. Escrowly enables Bitcoin escrow service along with Stable coins escrow service between individuals and businesses and among peer-to-peer entities:</p>
-                                        <ol>
-                                            <li>1. Ethereum escrow service for dApps, ICOs, and token launches.</li>
-                                            <li>2. USDT escrow service for stable, fast crypto payments.</li>
-                                            <li>3. Secure smart contract crypto escrow for personal or business transactions.</li>
-                                            <li>4. Escrow wallet for crypto until agreed contractual conditions have been fulfilled.</li>
-                                        </ol>
+                                        <ul>
+                                            <li>Ethereum escrow service for dApps, ICOs, and token launches.</li>
+                                            <li>USDT escrow service for stable, fast crypto payments.</li>
+                                            <li>Secure smart contract crypto escrow for personal or business transactions.</li>
+                                            <li>Escrow wallet for crypto until agreed contractual conditions have been fulfilled.</li>
+                                        </ul>
                                         <p>Escrowly provides dependable tracking alongside full transparency on how to use escrow for crypto or require crypto escrow for businesses.</p>
                                         <blockquote>Escrowly.com allows all legal businesses to accept payments via crypto, either directly from customers or by connecting its simple API to start collecting crypto payments from clients directly from their websites like Wordpress or Shopify etc.</blockquote>
                                         <h3>NFTs and DeFi</h3>
@@ -66,12 +66,12 @@ const AllowedGoodsAndServices = () => {
                                         <p>Our platform simplifies trading operations while providing worldwide safety assurance for buyers, sellers, investors and platforms in the ever-growing Web3 world.</p>
                                         <h3>Real Estate & Property Deals</h3>
                                         <p>Escrowly.com caters to real estate closings, property sales, and rentals, particularly when dealing with cross-border crypto escrow.</p>
-                                        <ol>
-                                            <li>1. Escrow for property sales with crypto.</li>
-                                            <li>2. Escrow for rental property deposits.</li>
-                                            <li>3. Escrow for land and property auctions.</li>
-                                            <li>4. Escrow for franchise business purchases.</li>
-                                        </ol>
+                                        <ul>
+                                            <li>Escrow for property sales with crypto.</li>
+                                            <li>Escrow for rental property deposits.</li>
+                                            <li>Escrow for land and property auctions.</li>
+                                            <li>Escrow for franchise business purchases.</li>
+                                        </ul>
                                         <p>Our escrow for international business deals and corporate mergers eliminates wire fraud and contract disputes through fast and precise services.</p>
                                         <h3>Cars, Equipment & High-Value Goods</h3>
                                         <p>Big-ticket items require big-time trust. Escrowly.com handles:</p>
