bf11e05aefb62ab4abf4a9f17fdd5e949b33dafd
dacentra <fahad@dacentra.com>
2025-04-14 08:42:33 +0000

    payoption20

--- src/pages/WhatIsEscrowly/DisbursementsMethods.jsx
+++ src/pages/WhatIsEscrowly/DisbursementsMethods.jsx
@@ -96,7 +96,7 @@ const DisbursementsMethods = () => {
                                         <p>The company operates on audited smart contracts and safe digital storage systems. Our technology suits more than 30 specific situations and works best when used for escrow for jewelry and luxury watches, escrow for rental property deposits, and escrow for event ticket sales. Escrowly is perfect for escrow for international property sales and cross-border crypto escrow.</p>
                                         <h3>Get Paid the Escrowly Way</h3>
                                         <p>Escrowly helps users avoid payment trouble and protects them from missing crypto transfers. Our payment distribution system uses next-generation technologies for international business deals.</p>
-                                        <h6>Are you ready to get started? Open Escrowly account to enjoy safe crypto escrow protection deals with guaranteed and quick payments.</h6>
+                                        <h6>Open Escrowly account to enjoy safe crypto escrow protection deals as Escrowly proudly benchmark being the best online escrow service in the world.</h6>
                                     </div>
                                 </div>
                                 <div className="col col-right">
