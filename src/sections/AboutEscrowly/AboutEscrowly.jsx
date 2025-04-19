776eb484ef79c1f31300a45b773b517a8f5ed54e
dacentra <fahad@dacentra.com>
2025-04-19 20:21:30 +0800

    start8

--- src/sections/AboutEscrowly/AboutEscrowly.jsx
+++ src/sections/AboutEscrowly/AboutEscrowly.jsx
@@ -37,8 +37,8 @@ const AboutEscrowly = () => {
                                 <h5>Our Purpose</h5>
                                 <p>Our organization aims to establish a secure, intelligent crypto economy that enables every person to perform transactions safely. Our team strives to build technologies that help create an electronic payment system which functions instantaneously, provides absolute security, and is generally accepted worldwide.</p>   
                                 <Button 
-                                    url="#get-started"
-                                    text="Get Started"
+                                    url="/sign-in"
+                                    text="Start A Transaction"
                                     icon="/img/btn-arrow-right.svg"
                                     delay={0}
                                 />    
