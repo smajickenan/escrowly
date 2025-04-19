930f632f47d8a0ecd723a95df59eb27145604301
dacentra <fahad@dacentra.com>
2025-04-19 12:09:19 +0800

    index.js-2

--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { HelmetProvider } from "react-helmet-async";
-import { BrowserRouter as Router } from 'react-router-dom';
 import "./styles/style.scss";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <HelmetProvider>
-        <Router>
-            <App />
-        </Router>
+        <App />
     </HelmetProvider>
 );
