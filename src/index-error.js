5951a9a4a3a17f7d02ffba3a87d100be3466cf67
dacentra <fahad@dacentra.com>
2025-04-19 12:02:58 +0800

    index.js-putback

--- src/index-error.js
+++ src/index-error.js
@@ -1,13 +1,16 @@
+// index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { BrowserRouter } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import "./styles/style.scss";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <HelmetProvider>
-        <App />
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
     </HelmetProvider>
 );
-
