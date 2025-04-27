402d6363d92e3ffc1126cefdab719636ee48fd49
dacentra <fahad@dacentra.com>
2025-04-27 17:17:07 +0800

    feesText2

--- src/sections/FeesTransactions/FeesTransactions.jsx
+++ src/sections/FeesTransactions/FeesTransactions.jsx
@@ -183,7 +183,7 @@ const FeesTransactions = function () {
                     )}
                 </div>
 
-                <div className="blog-article-content">
+                <div className="col col-left">
                     <h2>Fee Calculator</h2>
                     <h3>Instantly Estimate Escrow Fees for Your Secure Crypto Transactions</h3>
                     <p>
