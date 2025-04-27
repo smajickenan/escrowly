cdeb8425eb0fc4d0b21a5b51601b12c9a7f18bf3
dacentra <fahad@dacentra.com>
2025-04-27 17:56:14 +0800

    feesText10

--- src/sections/FeesTransactions/FeesTransactions.jsx
+++ src/sections/FeesTransactions/FeesTransactions.jsx
@@ -182,6 +182,7 @@ const FeesTransactions = function () {
                     </div>
                     )}
                 </div>
+                <div className="col col-left">
                 <div className="blog-article-content">
                     <h3>Instantly Estimate Escrow Fees for Your Secure Crypto Transactions</h3>
                     <p>If you want to enjoy secure P2P crypto escrow transactions, you should know how much it costs to run your transactions. Users can get cost estimates before starting their secure crypto trade with Escrowly's Fee Calculator. Through its Fee Calculator system, Escrowly provides real-time estimates of payment costs that eliminate secret costs or unexpected charges.</p>
@@ -234,6 +235,7 @@ const FeesTransactions = function () {
                     </ul>
                     <p className="call-to-action">Use the Escrowly Fee Calculator</p>
                     <p className="tagline">Plan Smarter. Trade Safer. Only Pay What's Fair.</p>
+                </div>
                 </div>
             </div>
         </section>
