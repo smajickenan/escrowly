e06aab78ef061933a2a19b24e26a3532f597c3e9
dacentra <fahad@dacentra.com>
2025-04-28 12:41:28 +0800

    pi1

--- src/pages/PartnersEnquiry.jsx
+++ src/pages/PartnersEnquiry.jsx
@@ -35,31 +35,122 @@ const PartnersEnquiry = () => (
                             <span className="post-date">February 10, 2024</span>
                             <div className="span time-to-read"><img src="/img/icon-time.svg" alt="" />4 min read</div>
                         </div>
-                        <div className="blog-article-content">
-                            <p>In today’s fast-paced digital economy, security in financial transactions is more critical than ever. Whether you're a freelancer, business owner, or online seller, ensuring that payments are processed safely and fairly is essential. Escrowly offers a cutting-edge escrow service designed to protect both buyers and sellers in cryptocurrency transactions, eliminating risks and fostering trust.</p>
-                            <h3>How Escrowly Ensures Safe Transactions</h3>
-                            <p>Our platform follows a straightforward process to guarantee security for both buyers and sellers:</p>   
-                            <ol>
-                                <li>1. Transaction Agreement – The buyer and seller agree to the terms of the deal, ensuring clarity and mutual understanding</li>
-                                <li>2. Funds Secured – The buyer deposits cryptocurrency into our escrow system, keeping it securely stored</li>
-                                <li>3.Delivery & Confirmation – The seller delivers the product or service, and the buyer verifies satisfaction</li>
-                                <li>4. Funds Released – Once both parties confirm the terms are met, Escrowly transfers the funds to the seller, completing the transaction securely</li>
-                            </ol>
-                            <blockquote>At Escrowly, we prioritize security and transparency in digital transactions. By leveraging cutting-edge escrow technology, we protect buyers and sellers from fraud, ensuring every payment is safe, verified, and reliable. Join us in building a more secure digital economy</blockquote>
-                            <p>Escrowly provides a seamless and transparent way to conduct financial transactions, ensuring that funds are held securely until both parties fulfill their agreed-upon terms. By integrating an escrow system with cryptocurrency, we make digital payments safer, more efficient, and more reliable.</p>
-                            <h4>Benefits of Using Escrowly</h4>
-                            <p>Escrowly offers a reliable and secure platform for businesses and individuals engaged in online transactions. By utilizing advanced technologies and best practices, Escrowly ensures that all parties involved in a transaction can have peace of mind. Here’s how Escrowly protects your interests:</p>
-                            <ul>
-                                <li>Fraud Prevention: Ensures that payments are only processed when conditions are met.</li>
-                                <li>Secure Crypto Transactions: Reduces the risks of scams in the volatile crypto market.</li>
-                                <li>Trust & Transparency: Builds confidence between buyers and sellers, fostering long-term business relationships.</li>
-                                <li>Global Reach: Allows businesses and freelancers to operate securely across international borders.</li>
-                                <li>Automated Process: Simplifies complex transactions with a user-friendly system.</li>
-                            </ul>
-                            <h6>Future-Proof Your Transactions with Escrowly</h6>
-                            <p>As digital transactions continue to evolve, businesses and individuals need a secure and reliable payment system that adapts to changing financial landscapes. Escrowly is committed to providing the highest level of security for cryptocurrency transactions, ensuring that your payments are protected every step of the way.</p>
-                            <p>Ready to safeguard your digital transactions? Explore Escrowly today and experience the future of secure online payments.</p>
-                        </div>
+                        <form
+      className="salesForm"
+      data-action="/api/sales"
+      data-component="userSubmittedForm"
+      noValidate
+      data-form-name="sales-form"
+      data-success-modal="modal-sales"
+      role="form"
+      aria-label="Sales form"
+    >
+      <div className="form-errors" data-target="form-error"></div>
+
+      <fieldset>
+        <legend>New partner enquiry form</legend>
+
+        <div className="form-group">
+          <div className="field" data-target="field" data-field="title">
+            <label htmlFor="field-title">
+              Title
+            </label>
+            <div>
+              <select id="field-title" name="title" data-target="field-focusable">
+                <option value="Mr">Mr.</option>
+                <option value="Mrs">Mrs.</option>
+                <option value="Ms">Ms.</option>
+                <option value="Mx">Mx</option>
+                <option value="Miss">Miss</option>
+                <option value="Mister">Mister</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="field" data-target="field" data-field="first-name">
+            <label htmlFor="field-first-name">
+              First name <sup>*</sup>
+            </label>
+            <div>
+              <input
+                type="text"
+                id="field-first-name"
+                name="first-name"
+                required
+                aria-describedby="error-first-name"
+                data-max-length="100"
+                autoComplete="off"
+                data-target="field-focusable"
+              />
+            </div>
+            <div className="field-error" data-target="field-error" data-attr="error-invalid">
+              <span id="error-first-name">Please enter your first name.</span>
+            </div>
+          </div>
+
+          <div className="field" data-target="field" data-field="last-name">
+            <label htmlFor="field-last-name">
+              Surname <sup>*</sup>
+            </label>
+            <div>
+              <input
+                type="text"
+                id="field-last-name"
+                name="last-name"
+                required
+                aria-describedby="error-last-name"
+                data-max-length="100"
+                autoComplete="off"
+                data-target="field-focusable"
+              />
+            </div>
+            <div className="field-error" data-target="field-error" data-attr="error-invalid">
+              <span id="error-last-name">Please enter your surname.</span>
+            </div>
+          </div>
+
+          <div className="field" data-target="field" data-field="email-address">
+            <label htmlFor="field-email-address">
+              What is your email address? <sup>*</sup>
+            </label>
+            <div>
+              <input
+                type="email"
+                id="field-email-address"
+                name="email-address"
+                required
+                aria-describedby="error-email-address"
+                autoComplete="off"
+                data-target="field-focusable"
+              />
+            </div>
+            <div className="field-error" data-target="field-error" data-attr="error-invalid">
+              <span id="error-email-address">Please enter a valid email address.</span>
+            </div>
+          </div>
+
+          <div className="field" data-target="field" data-field="phone">
+            <label htmlFor="field-phone">
+              Phone number <sup>*</sup>
+            </label>
+            <div>
+              <input
+                type="text"
+                id="field-phone"
+                name="phone"
+                required
+                aria-describedby="error-phone"
+                autoComplete="off"
+                data-target="field-focusable"
+              />
+            </div>
+            <div className="field-error" data-target="field-error" data-attr="error-invalid">
+              <span id="error-phone">Please enter a valid phone number.</span>
+            </div>
+          </div>
+        </div>
+      </fieldset>
+    </form>
                     </div>
                 </section> 
             </main>
